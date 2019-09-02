0a7c79e48c3a4bb1536f9899a4a74491201136bf
Mike Keir <mikeyxote@yahoo.com>
2019-09-02 12:36:17 +0000

    add second fix to efforts migration

--- db/migrate/20190826220559_create_efforts.rb
+++ db/migrate/20190826220559_create_efforts.rb
@@ -2,7 +2,7 @@ class CreateEfforts < ActiveRecord::Migration
   def change
     create_table :efforts do |t|
       t.references :user, index: true, foreign_key: true
-      t.references :segment_id, index: true, foreign_key: true
+      t.references :segment, index: true, foreign_key: true
       t.float :start_date
       t.integer :elapsed_time
       t.bigint :strava_id
