070bccf3b657f11625a013544724785917e6d654
Mike Keir <mikeyxote@yahoo.com>
2019-09-03 01:14:01 +0000

    add perform_deliveries True to application_mailer

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,6 @@
 class ApplicationMailer < ActionMailer::Base
-  default from: "from@example.com"
+  default from: "jersey-tracker@heroku.com"
   layout 'mailer'
+  
+  mail.perform_deliveries = true
 end
