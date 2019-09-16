0a2cb4018e62af6d3ac0a4db3d46e654d3938e80
Mike Keir <mikeyxote@yahoo.com>
2019-09-16 15:10:56 +0000

    Add strava links to segments

--- app/helpers/segments_helper.rb
+++ app/helpers/segments_helper.rb
@@ -1,2 +1,5 @@
 module SegmentsHelper
+  def strava_for_segment(segment)
+    link_to "STRAVA", "https://www.strava.com/segments/" + segment.strava_id.to_s, target: "_blank", class: "btn btn-sm btn-strava"
+  end
 end
