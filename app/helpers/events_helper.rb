bc934cb24c5b6a6cec425a0dc281d0d78b08bcfa
Mike Keir <mikeyxote@yahoo.com>
2019-09-21 19:27:18 +0000

    Trophy icons added

--- app/helpers/events_helper.rb
+++ app/helpers/events_helper.rb
@@ -1,2 +1,10 @@
 module EventsHelper
+  
+  def show_trophy points
+    images = ['https://bucketroyal.s3.amazonaws.com/JerseyTracker/bronze_trophy.png',
+    'https://bucketroyal.s3.amazonaws.com/JerseyTracker/silver_trophy.png',
+    'https://bucketroyal.s3.amazonaws.com/JerseyTracker/gold_trophy.png']
+    image_tag(images[points-1], options: {height: 15, width: 15})
+  end
+  
 end
