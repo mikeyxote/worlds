f9cd09dfa19d2fbc153512b537709bf7d5181cea
Mike Keir <mikeyxote@yahoo.com>
2019-09-22 18:25:58 +0000

    Add default date and create from recommendation-date to new events

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,7 @@
 module ApplicationHelper
-  def datepicker_input form, field
+  def datepicker_input form, field, st
     content_tag :td, :data => {:provide => 'datepicker', 'date-format' => 'yyyy-mm-dd', 'date-autoclose' => 'true'} do
-      form.text_field field, class: 'form-control', placeholder: 'YYYY-MM-DD'
+      form.text_field field, class: 'form-control', placeholder: 'YYYY-MM-DD', value: st
     end
   end
 end
