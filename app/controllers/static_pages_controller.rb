9463eaa2a49d4a3a145097aa02ae0564129a1425
Mike Keir <mikeyxote@yahoo.com>
2019-09-22 18:01:57 +0000

    fix data retrieval error on homepage if user hasn't connected to Strava yet.

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -3,8 +3,10 @@ class StaticPagesController < ApplicationController
     if current_user
       @managing = current_user.managing
       @events = current_user.participating_in
-      @points = Point.where(user_id: current_user.id).sum(:val)
-      @new_activities = current_user.get_new_activities
+      if current_user.has_strava?
+        @points = Point.where(user_id: current_user.id).sum(:val)
+        @new_activities = current_user.get_new_activities
+      end
     else
       @managing = nil
       @events = nil
