23db991833749352b1d8aa6635662db8e7e07c7d
Mike Keir <mikeyxote@yahoo.com>
2019-09-25 22:39:16 +0000

    Add size option to profile_for

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,15 +1,18 @@
 module UsersHelper
   
-  def profile_for(user, options = { medium: true })
+  def profile_for(user, options = { medium: true, size: 100})
+    size = options['size'].to_s
     if options[:medium] == true
       profile_url = user.profile_medium
     else
       profile_url = user.profile
     end
-    image_tag(profile_url, alt: user.username, class: "gravatar")
+    image_tag(profile_url, :size => "#{size}x#{size}",
+                          class: "gravatar")
   end
   
   def strava_for(user)
-    link_to "STRAVA!", "https://www.strava.com/athletes/" + user.strava_id.to_s, target: "_blank", class: "btn btn-sm btn-strava"
+    link_to "STRAVA!", "https://www.strava.com/athletes/" + 
+    user.strava_id.to_s, target: "_blank", class: "btn btn-sm btn-strava"
   end
 end
