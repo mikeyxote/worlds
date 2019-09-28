77913b149c416bafccb0aa592282b94fa9086f35
Mike Keir <mikeyxote@yahoo.com>
2019-09-28 19:54:57 +0000

    Small changes

--- app/helpers/events_helper.rb
+++ app/helpers/events_helper.rb
@@ -4,6 +4,7 @@ module EventsHelper
     images = ['https://bucketroyal.s3.amazonaws.com/JerseyTracker/bronze_trophy.png',
     'https://bucketroyal.s3.amazonaws.com/JerseyTracker/silver_trophy.png',
     'https://bucketroyal.s3.amazonaws.com/JerseyTracker/gold_trophy.png']
+    
     image_tag(images[points-1], options: {height: 15, width: 15})
   end
   
