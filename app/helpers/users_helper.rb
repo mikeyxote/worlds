f09776994a8ed72afbc96916d3a1b4fbf9f0ea9f
Mike Keir <mikeyxote@yahoo.com>
2019-10-07 19:19:17 +0000

    clean up users index and cards with css flexbox

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -13,6 +13,6 @@ module UsersHelper
   
   def strava_for(user)
     link_to "STRAVA!", "https://www.strava.com/athletes/" + 
-    user.strava_id.to_s, target: "_blank", class: "btn btn-sm btn-strava"
+    user.strava_id.to_s, target: "_blank", class: "btn btn-sm btn-strava btn-pop"
   end
 end
