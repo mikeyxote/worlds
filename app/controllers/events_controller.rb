1ea5c54d1761d08b34f3cfca8ca8951e4c203635
Mike Keir <mikeyxote@yahoo.com>
2019-10-08 22:54:41 +0000

    add small fixes to event edit-> series addition

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -60,6 +60,7 @@ class EventsController < ApplicationController
   def update
     respond_to do |format|
       if @event.update(event_params)
+        @event.update(series_id: params['event']['series'])
         format.html { redirect_to @event, notice: 'Event was successfully updated.' }
         format.json { render :show, status: :ok, location: @event }
       else
